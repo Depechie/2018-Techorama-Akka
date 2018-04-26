6905980c645a5843a83d6c8426d31803a7abf00e
Depechie <glenn.versweyveld@gmail.com>
2018-04-26 23:27:10 +0200

    Step 07 : Use Poison Pill

--- AkkaNetSampleStd/Actors/ResultDispatcherActor.cs
+++ AkkaNetSampleStd/Actors/ResultDispatcherActor.cs
@@ -9,6 +9,7 @@ namespace AkkaNetSampleStd.Actors
     public class ResultDispatcherActor : ReceiveActor
     {
         private MainViewModel _viewModel;
+        private int _messageCount = 0;
 
         //Thanks to the fact that an Actor can only handle ONE message at a time from it's inbox, we don't have to worry about flooding the Observable Collection with concurrency problems
         public ResultDispatcherActor(MainViewModel viewModel)
@@ -19,9 +20,16 @@ namespace AkkaNetSampleStd.Actors
 
         private void HandleResult(TargetLinks msg)
         {
-            System.Diagnostics.Debug.WriteLine($"*** RESULT - {msg.Title}");
+            ++_messageCount;
 
-            Device.BeginInvokeOnMainThread(() => _viewModel.Results.Add(msg));
+            //When we received 5 results, trigger a stop in the message stream by sending a Poison Pill
+            if (_messageCount > 5)
+                Context.ActorSelection(Self.Path.Root).Tell(PoisonPill.Instance);
+            else
+            {
+                System.Diagnostics.Debug.WriteLine($"*** RESULT - {_messageCount} - {msg.Title}");
+                Device.BeginInvokeOnMainThread(() => _viewModel.Results.Add(msg));
+            }
         }
     }
 }
