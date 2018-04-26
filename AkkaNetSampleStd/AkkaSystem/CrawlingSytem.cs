40f264e34fec5aa6bf2ecf115f086bca8db10d72
Depechie <glenn.versweyveld@gmail.com>
2018-04-26 23:44:06 +0200

    Step 08 : Add DeadLetter monitor

--- AkkaNetSampleStd/AkkaSystem/CrawlingSytem.cs
+++ AkkaNetSampleStd/AkkaSystem/CrawlingSytem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Akka.Actor;
+using Akka.Event;
 using AkkaNetSampleStd.Actors;
 using AkkaNetSampleStd.Messages;
 using AkkaNetSampleStd.ViewModels;
@@ -10,31 +11,37 @@ namespace AkkaNetSampleStd.AkkaSystem
     {
         private static readonly ActorSystem _system;
         private static readonly IActorRef _coordinator;
+        private static readonly IActorRef _deadLettersSubscriber;
 
         static CrawlingSystem()
         {
             //Define a name for the Akka System
             _system = ActorSystem.Create("CrawlingSystem");
             _coordinator = _system.ActorOf(Props.Create<CoordinatorActor>(), "Coordinator");
+            _deadLettersSubscriber = _system.ActorOf<DeadLetterMonitor>("DeadLetterSubscriber");
         }
 
         public static void StartCrawling(string url, MainViewModel viewModel)
-        {
+        {            
             var props = Props.Create(() => new ResultDispatcherActor(viewModel));
             var dispatcher = _system.ActorOf(props);
 
             //Subscribe to the Pub/Sub event to get notified when a new result is available, pass it through to the dispatcher actor
             _system.EventStream.Subscribe(dispatcher, typeof(TargetLinks));
+            _system.EventStream.Subscribe(_deadLettersSubscriber, typeof(DeadLetter));
 
             //Kick off the first crawl through use of Akka.Net
             _coordinator.Tell(new Target() { Url = url });
         }
 
-        public static void Stop()
+        public static async void Stop()
         {
             //Let the actor execute the message currently being executed before shutting down the whole actor instance
             //This means that no other messages will be executed, including those that are enqueued before the actual Stop() method is invoked! ( We should see dead letters in the log )
             _system.Stop(_coordinator);
+
+            await System.Threading.Tasks.Task.Delay(TimeSpan.FromSeconds(30));
+
             _system.Terminate();
         }
     }
