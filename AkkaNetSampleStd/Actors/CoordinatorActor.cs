984c4041bbbdcef74c038b5571baade26a63d9d9
Depechie <glenn.versweyveld@gmail.com>
2018-04-26 22:34:54 +0200

    Step 03 : Add actors

---
+++ AkkaNetSampleStd/Actors/CoordinatorActor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Routing;
+using AkkaNetSampleStd.Messages;
+
+namespace AkkaNetSampleStd.Actors
+{
+    public class CoordinatorActor : ReceiveActor
+    {
+        private readonly IActorRef _crawlers;
+
+        public CoordinatorActor()
+        {
+            _crawlers = Context.ActorOf(Props.Create(() => new CrawlActor(Self)));
+
+            Receive<Target>(msg => _crawlers.Tell(msg));
+            Receive<TargetLinks>(msg => OnReceiveTargetLinks(msg));
+        }
+
+        private void OnReceiveTargetLinks(TargetLinks result)
+        {
+            //Publish the actual result onto the build in Pub/Sub channel
+            if (!string.IsNullOrWhiteSpace(result.Title))
+                Context.System.EventStream.Publish(result);
+            
+            //When a crawl result comes in, pass new links to the actor pool to trigger new crawls
+            foreach (var url in result.LinkedUrls)
+                _crawlers.Tell(new Target() { Url = url });
+        }
+    }
+}
---
+++ AkkaNetSampleStd/Actors/ResultDispatcherActor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Akka.Actor;
+using AkkaNetSampleStd.Messages;
+using AkkaNetSampleStd.ViewModels;
+using Xamarin.Forms;
+
+namespace AkkaNetSampleStd.Actors
+{
+    public class ResultDispatcherActor : ReceiveActor
+    {
+        private MainViewModel _viewModel;
+
+        //Thanks to the fact that an Actor can only handle ONE message at a time from it's inbox, we don't have to worry about flooding the Observable Collection with concurrency problems
+        public ResultDispatcherActor(MainViewModel viewModel)
+        {
+            _viewModel = viewModel;
+            Receive<TargetLinks>(msg => HandleResult(msg));
+        }
+
+        private void HandleResult(TargetLinks msg)
+        {
+            System.Diagnostics.Debug.WriteLine($"*** RESULT - {msg.Title}");
+
+            Device.BeginInvokeOnMainThread(() => _viewModel.Results.Add(msg));
+        }
+    }
+}
