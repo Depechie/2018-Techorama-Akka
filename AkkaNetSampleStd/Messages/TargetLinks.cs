075896e74d35efad56479670e6f3957884cb1da6
Depechie <glenn.versweyveld@gmail.com>
2018-04-26 22:32:08 +0200

    Step 02 : Add messages

---
+++ AkkaNetSampleStd/Messages/TargetLinks.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AkkaNetSampleStd.Messages
+{
+    public class TargetLinks
+    {
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public IList<string> LinkedUrls { get; set; }
+    }
+}
