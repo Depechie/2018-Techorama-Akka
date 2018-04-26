5f4335ef53bba1253ee62a4660ebb5a2bd26bd98
Depechie <glenn.versweyveld@gmail.com>
2018-04-26 22:29:30 +0200

    Step 01 : UI setup

---
+++ AkkaNetSampleStd/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AkkaNetSampleStd.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _url;
+        public string Url
+        {
+            get => _url;
+            set => SetProperty(ref _url, value);
+        }
+
+        private ICommand _startCrawlingCommand;
+        public ICommand StartCrawlingCommand => _startCrawlingCommand ?? (_startCrawlingCommand = new Command(HandleAction));
+
+        private void HandleAction()
+        {
+        }
+    }
+}
---
+++ AkkaNetSampleStd/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using AkkaNetSampleStd.ViewModels;
+using Xamarin.Forms;
+
+namespace AkkaNetSampleStd.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
