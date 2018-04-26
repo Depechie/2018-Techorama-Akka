898eb7055e91bf6d62b687588a6c47c45a399086
Depechie <glenn.versweyveld@gmail.com>
2018-04-26 23:44:06 +0200

    Step 08 : Add DeadLetter monitor

---
+++ AkkaNetSampleStd/Actors/DeadLetterMonitor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Akka.Actor;
+using Akka.Event;
+
+namespace AkkaNetSampleStd.Actors
+{
+    public class DeadLetterMonitor : ReceiveActor
+    {
+        public DeadLetterMonitor()
+        {
+            Receive<DeadLetter>(arg => HandleDeadLetter(arg));
+        }
+
+        private void HandleDeadLetter(DeadLetter arg)
+        {
+            System.Diagnostics.Debug.WriteLine($"Msg {arg.Message} \n Sender {arg.Sender} \n Recipient {arg.Recipient}");
+        }
+    }
+}
